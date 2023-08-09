fe0bd004142e188177fcbaea8bbe59745b47b768
Joao Pedro <joao.braga.porto01@gmail.com>
2023-08-08 22:04:00 -0200

    build: add next-auth, prisma and bcypt
    I add next-auth with session provider, add prisma schema and connected with mongodb and add bcrypt
    to hash the passwords

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,53 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from 'bcrypt'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import prisma from '../../../libs/prisma'
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error('Invalid credentials')
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if (!user || !user?.hashedPassword) {
+                    throw new Error('Usuário não encontrado')
+                }
+
+                const isCorretPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+                if (!isCorretPassword) {
+                    throw new Error('Senha incorreta')
+                }
+
+                return user
+            }
+
+
+        })
+    ],
+    pages: {
+        signIn: '/login'
+    },
+    debug: process.env.NODE_ENV === 'development',
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+}
+export default NextAuth(authOptions)
